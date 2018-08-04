6e5abef59603db12b2e5b01f1f1bcf23d958b030
Dcroix <pghilado@up.edu.ph>
2018-08-04 21:06:23 +0800

    Updated package description for version 2.0 and included sample dataset for practice

--- R/sampleElements.R
+++ R/sampleElements.R
@@ -1,4 +1,4 @@
-#Created by Paolo G. Hilado July 20, 2018
+# This file is part of the RSamplingz package with License GPL-3.0-or-later
 sampleElements<- function(file, sheet, N, n, s){
   library(xlsx)
   data <- read.xlsx(file, sheet)
@@ -7,4 +7,6 @@ sampleElements<- function(file, sheet, N, n, s){
   print(list)
 }
 
-#"CSAB Employees.xlsx", "FullTime"
+#Sample Data included in the package: "NamesList.xlsx" with sheet name "Names";
+#150 observations with 3 variables: 1) Code / 2) Surname / 3) FirstName
+#Make sure the Sample Data or your Data to be processed is in the working directory
--- R/sysElements.R
+++ R/sysElements.R
@@ -1,4 +1,4 @@
-#Created by Paolo G. Hilado July 20, 2018
+# This file is part of the RSamplingz package with License GPL-3.0-or-later
 sysElements <- function(file, sheet, N, n, s){
   library(xlsx)
   data <- read.xlsx(file, sheet)
@@ -7,3 +7,6 @@ sysElements <- function(file, sheet, N, n, s){
   print(list)
 }
 
+#Sample Data included: "NamesList.xlsx" with sheet name "Names";
+#150 observations with 3 variables: 1) Code / 2) Surname / 3) FirstName
+#Make sure the Sample Data or your Data to be processed is in the working directory
